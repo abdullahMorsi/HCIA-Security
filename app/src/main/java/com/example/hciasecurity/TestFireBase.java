a9a0978da8036fd9bc15576bda71ab05ae5d7c9b
abdullahMorsi <abdoalnegm69@gmail.com>
2020-09-26 16:07:23 +0200

    finishe firebase retrieval

--- app/src/main/java/com/example/hciasecurity/TestFireBase.java
+++ app/src/main/java/com/example/hciasecurity/TestFireBase.java
@@ -1,6 +1,7 @@
 package com.example.hciasecurity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.solver.widgets.Snapshot;
 
@@ -9,8 +10,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hciasecurity.Logic.MyQuestions;
+import com.example.hciasecurity.Logic.Questions.MultipleChoice;
 import com.example.hciasecurity.Logic.Questions.SingleChoice;
 import com.example.hciasecurity.Logic.Questions.TrueAndFalse;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,27 +24,36 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class TestFireBase extends AppCompatActivity {
-    TextView textView;
+    TextView textView,textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fire_base);
 
         textView=findViewById(R.id.editTextTextMultiLine);
+        textView2=findViewById(R.id.textView2);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference=database.getReference();
         DatabaseReference myRef = database.getReference();
 
-        final TrueAndFalse trueAndFalse =new TrueAndFalse("The question",true,0);
-        trueAndFalse.setPoints(15);
-        trueAndFalse.setImg_url("the url");
-        myRef.child("True and false question").setValue(trueAndFalse);
-        myRef.child("True and false question").addListenerForSingleValueEvent(new ValueEventListener() {
+//        final TrueAndFalse trueAndFalse =new TrueAndFalse("The question",true,0);
+//        trueAndFalse.setPoints(15);
+//        trueAndFalse.setImg_url("the url");
+//        myRef.child("True and false question").setValue(trueAndFalse);
+
+        final ArrayList<TrueAndFalse>trueAndFalses =new ArrayList<>();
+
+        myRef.child("True and False question").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                TrueAndFalse trueAndFalse1=snapshot.getValue(TrueAndFalse.class);
-                textView.setText(trueAndFalse.toString());
+                for (DataSnapshot snapshot1:snapshot.getChildren()) {
+                    TrueAndFalse trueAndFalse1=snapshot1.getValue(TrueAndFalse.class);
+                    trueAndFalses.add(trueAndFalse1);
+                }
+                MyQuestions questions=new MyQuestions();
+                questions.setTrueAndFalses(trueAndFalses);
+//                textView.setText(questions.toString());
             }
 
             @Override
@@ -50,22 +62,28 @@ public class TestFireBase extends AppCompatActivity {
             }
         });
 
-        SingleChoice singleChoice=new SingleChoice("The question", "correct answer",
-                new LinkedList<String>(){{
-                    add("wrong answer 1");
-                    add("wrong answer 2");
-                }},0);
-        databaseReference.child("my single").setValue(singleChoice);
+
+//        final SingleChoice singleChoice=new SingleChoice("The question", "correct answer",
+//                new LinkedList<String>(){{
+//                    add("wrong answer 1");
+//                    add("wrong answer 2");
+//                }},0);
+//
+//        databaseReference.child("my single").setValue(singleChoice);
+
         final ArrayList<SingleChoice>singleChoices =new ArrayList<>();
-        databaseReference.child("my single").addListenerForSingleValueEvent(new ValueEventListener() {
+
+        databaseReference.child("Single Question").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                for(DataSnapshot snapshot1:snapshot.getChildren()){
-                   SingleChoice singleChoice=snapshot.getValue(SingleChoice.class);
+                   SingleChoice singleChoice=snapshot1.getValue(SingleChoice.class);
                    singleChoices.add(singleChoice);
                }
                 MyQuestions questions=new MyQuestions();
-               questions.setSingleChoices(singleChoices);
+                questions.setSingleChoices(singleChoices);
+                textView2.setText(questions.toString());
+
             }
 
             @Override
@@ -73,5 +91,40 @@ public class TestFireBase extends AppCompatActivity {
 
             }
         });
+
+//        final MultipleChoice multipleChoice = new MultipleChoice("The Question", new LinkedList<String>(){
+//            {
+//                add("correct 1");
+//                add("correct 2");
+//            }
+//        }, new LinkedList<String>(){
+//            {
+//                add("wrong 1");
+//                add("wrong 2");
+//            }
+//        }, 0);
+//
+//        databaseReference.child("Multi").setValue(multipleChoice);
+
+        final ArrayList<MultipleChoice> multipleChoices = new ArrayList<>();
+
+        databaseReference.child("Multiple Question").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1:snapshot.getChildren()) {
+                    MultipleChoice multipleChoice1 = snapshot1.getValue(MultipleChoice.class);
+                    multipleChoices.add(multipleChoice1);
+                }
+                MyQuestions questions = new MyQuestions();
+                questions.setMultipleChoices(multipleChoices);
+                textView.setText(questions.toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
